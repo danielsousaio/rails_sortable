dc7e5fd6038e6835093dd528ffcea5ea7713a4e4
Daniel Sousa <mail@danielsousa.io>
2017-05-25 11:52:07 +0100

    Add new sortable fetch for arrays first element

--- app/helpers/sortable_helper.rb
+++ app/helpers/sortable_helper.rb
@@ -6,4 +6,11 @@ module SortableHelper
       yield object, "#{object.class}_#{object.id}"
     end
   end
+
+  def sortable_fetch_array(models, &block)
+    raise "You must call with block!" unless block_given?
+    models.each do |object|
+      yield object, "#{object.class}_#{object.first.id}"
+    end
+  end
 end
